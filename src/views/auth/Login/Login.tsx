004c4c03dcb4e6543fb775d837189bf42ffc6a39
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-21 22:24:25 +0530

    added form container on login.tsx

--- src/views/auth/Login/Login.tsx
+++ src/views/auth/Login/Login.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { FormContainer } from '../../../components/FormContainer';
 
 interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = ({}) => {
-  return <></>;
+  return (
+    <FormContainer color='#bbbbbb'>
+      <h1>hello</h1>
+    </FormContainer>
+  );
 };
