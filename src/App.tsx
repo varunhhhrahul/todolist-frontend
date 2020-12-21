89d82b10793fe995b98b57886ec336361cef23f5
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-21 21:53:27 +0530

    added routes for login and register

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import logo from './logo.svg';
 import { MuiThemeProvider as ThemeProvider } from '@material-ui/core';
 import { MuiTheme } from './theme';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import store from './store';
 import { loadUser } from './slices/authSlice';
 import setAuthToken from './utils/setAuthToken';
+import { Alert } from '@material-ui/lab';
+import MyAlert from './components/Alert';
+import { LOGIN, REGISTER } from './constants/routes';
+import { Login } from './views/auth/Login/Login';
+import { Register } from './views/auth/Register/Register';
 
 const App = () => {
   useEffect(() => {
@@ -22,7 +28,18 @@ const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={MuiTheme}>
-        <></>
+        <Router>
+          <MyAlert />
+          {/* Add Suspense */}
+          <Switch>
+            <Route exact path={LOGIN}>
+              <Login />
+            </Route>
+            <Route exact path={REGISTER}>
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
       </ThemeProvider>
     </Provider>
   );
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,5 @@
 export const AUTH = '/auth';
-export const LOGIN = `${AUTH}/login`;
+export const LOGIN = `/`;
 export const REGISTER = `${AUTH}/register`;
 export const DASHBOARD = '/dashboard/todos';
 export const SINGLE_TODO = `${DASHBOARD}/:id`;
