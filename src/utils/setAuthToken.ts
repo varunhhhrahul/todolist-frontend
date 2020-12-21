a11fee8648d0ace45058b42fae8d550f2e347657
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-21 18:06:31 +0530

    added utild for authentication

---
+++ src/utils/setAuthToken.ts
@@ -0,0 +1,14 @@
+import API from '../api/api';
+const setAuthToken = async (token: string) => {
+  if (token) {
+    // Apply to every request
+    API.defaults.headers.common['Authorization'] = await `Bearer ${token}`;
+    localStorage.setItem('token', token);
+  } else {
+    // Delete auth header
+    await delete API.defaults.headers.common['Authorization'];
+    localStorage.removeItem('token');
+  }
+};
+
+export default setAuthToken;
