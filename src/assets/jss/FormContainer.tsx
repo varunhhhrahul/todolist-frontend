4f5bfde6b55070f5e8ff81f2b625f1ee6a9a5d14
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-21 19:33:01 +0530

    added formik for login and register forms

---
+++ src/assets/jss/FormContainer.tsx
@@ -0,0 +1,20 @@
+const formContainerStyles = (theme: any) => ({
+  root: {
+    '& > * ': {
+      marginTop: theme.spacing(3),
+      width: '25ch',
+    },
+  },
+  grid: {
+    minWidth: '350px',
+    maxWidth: '500px',
+    marginTop: '30px',
+    marginBottom: '40px',
+  },
+  paper: {
+    marginTop: theme.spacing(0),
+    padding: '15px',
+    borderRadius: '15px',
+  },
+});
+export default formContainerStyles;
