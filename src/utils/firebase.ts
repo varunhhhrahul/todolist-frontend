36b7691b5145b01c94d92dbb42e187c36d741b43
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-21 18:05:45 +0530

    added firebase config for OAuth

---
+++ src/utils/firebase.ts
@@ -0,0 +1,7 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAMCwSQbwRaU-6hZcQ6fs_OD_wZ-rhLrqA',
+  authDomain: 'todolist-2b114.firebaseapp.com',
+});
+export default firebase;
