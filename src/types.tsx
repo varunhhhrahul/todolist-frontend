31a35df6a270a3ce24f42c37960b9b33d7b239ea
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-21 18:04:09 +0530

    added few types for form data

---
+++ src/types.tsx
@@ -0,0 +1,15 @@
+export type registerFormData = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type loginFormData = {
+  email: string;
+  password: string;
+};
+
+// export type loginFormData = {
+//   email: string;
+//   password: string;
+// };
