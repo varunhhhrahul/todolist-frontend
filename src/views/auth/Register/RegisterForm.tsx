fa46efaadea2ecde6b9e58d32ae77b2f7fd1d76c
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-22 20:05:57 +0530

    basic setup done for register form

--- src/views/auth/Register/RegisterForm.tsx
+++ src/views/auth/Register/RegisterForm.tsx
@@ -1,7 +1,28 @@
+import { FormikProps } from 'formik';
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
+import { EnhancedRegisterFormValues } from './EnhancedRegisterForm';
 interface RegisterFormProps {}
 
-export const RegisterForm: React.FC<RegisterFormProps> = ({}) => {
-  return <></>;
+export const RegisterForm: React.FC<
+  RegisterFormProps & FormikProps<EnhancedRegisterFormValues>
+> = (props) => {
+  const dispatch = useDispatch();
+  const debug = false;
+  const {
+    values,
+    errors,
+    touched,
+    handleSubmit,
+    handleBlur,
+    handleChange,
+  } = props;
+  return (
+    <>
+      <div style={{ textAlign: 'center', color: 'white', marginBottom: 12 }}>
+        <h2>Sign Up</h2>
+      </div>
+      <form onSubmit={handleSubmit}></form>
+    </>
+  );
 };
