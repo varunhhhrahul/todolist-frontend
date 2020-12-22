35d102ee6a5756a961bccee176aeb5d7e4e298f2
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-22 20:04:45 +0530

    basic setup of login form

--- src/views/auth/Login/LoginForm.tsx
+++ src/views/auth/Login/LoginForm.tsx
@@ -3,5 +3,11 @@ import React from 'react';
 interface LoginFormProps {}
 
 export const LoginForm: React.FC<LoginFormProps> = ({}) => {
-  return <></>;
+  return (
+    <>
+      <div style={{ textAlign: 'center', color: 'white', marginBottom: 12 }}>
+        <h2>Login</h2>
+      </div>
+    </>
+  );
 };
