941589b1f867c9844337aeefd98744c34ad5c140
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-22 20:05:30 +0530

    added enhanced register form in register.tsx

--- src/views/auth/Register/Register.tsx
+++ src/views/auth/Register/Register.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import EnhancedRegisterForm from './EnhancedRegisterForm';
 
 interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = ({}) => {
-  return <></>;
+  return (
+    <>
+      <EnhancedRegisterForm />
+    </>
+  );
 };
