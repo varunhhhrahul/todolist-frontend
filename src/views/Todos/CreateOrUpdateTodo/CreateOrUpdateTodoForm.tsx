a7575b0246d0c29520dd521bd2f1c9d01589a165
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-23 21:38:42 +0530

    added create or update todo form

--- src/views/Todos/CreateOrUpdateTodo/CreateOrUpdateTodoForm.tsx
+++ src/views/Todos/CreateOrUpdateTodo/CreateOrUpdateTodoForm.tsx
@@ -1,7 +1,88 @@
+import { FormControl, TextField, Button } from '@material-ui/core';
+import { FormikProps } from 'formik';
 import React from 'react';
-
+import {
+  RootStateOrAny,
+  shallowEqual,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
+import { EnhancedCreateOrUpdateTodoFormValues } from './EnhancedCreateOrUpdateTodoForm';
 interface CreateOrUpdateTodoFormProps {}
 
-export const CreateOrUpdateTodoForm: React.FC<CreateOrUpdateTodoFormProps> = ({}) => {
-  return <></>;
+export const CreateOrUpdateTodoForm: React.FC<
+  CreateOrUpdateTodoFormProps &
+    FormikProps<EnhancedCreateOrUpdateTodoFormValues>
+> = (props) => {
+  const dispatch = useDispatch();
+
+  const debug = false;
+  const {
+    values,
+    errors,
+    touched,
+    handleSubmit,
+    handleBlur,
+    handleChange,
+  } = props;
+  const {
+    todo: { loading },
+  } = useSelector((state: RootStateOrAny) => {
+    return {
+      todo: state.todo,
+    };
+  }, shallowEqual);
+  return (
+    <>
+      <div style={{ paddingTop: '0.5rem' }}>
+        <h1>TO-DO:</h1>
+
+        <form onSubmit={handleSubmit}>
+          <FormControl style={{ width: '100%' }}>
+            <TextField
+              color='secondary'
+              variant='standard'
+              label='Task'
+              placeholder='Task'
+              name='task'
+              value={values.task}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={(errors.task && touched.task) || false}
+              helperText={errors.task && touched.task && errors.task}
+            />
+          </FormControl>
+          <FormControl style={{ width: '100%' }}>
+            <TextField
+              color='secondary'
+              variant='standard'
+              label='Description'
+              placeholder='Please add a task description'
+              name='description'
+              value={values.description}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={(errors.description && touched.description) || false}
+              helperText={
+                errors.description && touched.description && errors.description
+              }
+              multiline
+            />
+          </FormControl>
+
+          <Button
+            style={{
+              backgroundColor: '#48BAF8',
+              color: 'white',
+              marginTop: '1rem',
+            }}
+            variant='contained'
+            type='submit'
+          >
+            {loading ? 'Loading...' : 'Add new Todo'}
+          </Button>
+        </form>
+      </div>
+    </>
+  );
 };
