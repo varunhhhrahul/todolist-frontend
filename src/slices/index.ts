c44a721469dc216ad24c2a884800d4083da90c75
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-23 20:48:34 +0530

    added todo reducer in index slice

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authSlice';
 import alertReducer from './alertSlice';
+import todoReducer from './todoSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   alert: alertReducer,
+  todo: todoReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
