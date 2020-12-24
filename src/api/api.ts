33e47066501240c97e3704ebc0e54ff2c763577a
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-24 09:57:25 +0530

    minor changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL =
+  // 'http://localhost:5000/todolist-backend-15ebe/us-central1/app/api/v1';
   'https://us-central1-todolist-backend-15ebe.cloudfunctions.net/app/api/v1';
 
 const API = axios.create({
